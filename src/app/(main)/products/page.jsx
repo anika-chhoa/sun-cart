6483fba86ed2b96a2424e948d8fa2d7bc14e9885
anika-chhoa <chhoa.anika07@gmail.com>
2026-05-04 14:34:26 +0600

    add search bar and category section in products

--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -1,30 +1,48 @@
 import Categories from "@/components/Categories";
 import ProductCard from "@/components/ProductCard";
+import SearchBar from "@/components/SearchBar";
 import { getAllProducts } from "@/lib/data";
 
-const ProductsPage = async () => {
+const ProductsPage = async ({ searchParams }) => {
+  const { category, search } = await searchParams;
   const products = await getAllProducts();
+
+  const filteredProducts = products.filter((product) => {
+    const matchCategory = category
+      ? product.category.toLowerCase() === category.toLowerCase()
+      : true;
+
+    const matchSearch = search
+      ? product.name.toLowerCase().includes(search.toLowerCase())
+      : true;
+
+    return matchCategory && matchSearch;
+  });
 
   return (
     <div className="min-h-screen bg-[#fcf9f8]">
       <div className="max-w-7xl mx-auto px-6 md:px-12 py-12">
         <div className="mb-10 text-center">
-          <h1 className="text-3xl md:text-4xl font-extrabold tracking-tight text-base-content">
+          <h1 className="text-3xl md:text-4xl font-extrabold">
             All{" "}
             <span className="bg-gradient-to-r from-orange-500 to-amber-500 bg-clip-text text-transparent">
               Products
             </span>
           </h1>
-          <p className="text-base-content/50 text-sm mt-2 text-center">
+          <p className="text-base-content/50 text-sm mt-2">
             Premium summer essentials designed for your everyday moments.
           </p>
         </div>
-        <Categories/>
+
+        <SearchBar searchParams={searchParams} />
+
+        <Categories searchParams={searchParams} />
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
